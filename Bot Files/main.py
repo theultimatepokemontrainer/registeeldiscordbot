edeea6f4927db103bb63ac128b6dd17f22c9851e
theultimatepokemontrainer <80106955+theultimatepokemontrainer@users.noreply.github.com>
2021-05-08 21:25:47 +0800

    added the Go to: 7 Wonders of The World commands

--- Bot Files/main.py
+++ Bot Files/main.py
@@ -40,6 +40,7 @@ async def on_message(message):
 
     if message.content == ('r!help fun'):
         embedVar = discord.Embed(title="FUN Commands", description="`r!randomnumber, r!yt, r!say <message>, r!poll, r!fact`", color=0x1abc9c)
+        embedVar.add_field(name="Go to: 7 Wonders of The World", value="`r!goto taj mahal, r!goto colosseum, r!goto chichen itza, r!goto machu picchu, r!goto Christ the Redeemer (Note: the c and r has to be capital or it won't work), r!goto petra, r!goto great wall of china`")
         await message.channel.send(embed=embedVar)
 
     if message.content == ('r!help games'):
@@ -49,55 +50,76 @@ async def on_message(message):
         await message.channel.send(embed=embedVar)
 
     if message.content == ('r!help other'):
-        embedVar= discord.Embed(title="Other Commands", description="`r!randomquote, r!sourcecode, r!website, r!invite, r!vote, r!help`", color=0x1abc9c)
+        embedVar= discord.Embed(title="Other Commands", description="`r!quote, r!sourcecode, r!website, r!invite, r!vote, r!help`", color=0x1abc9c)
         await message.channel.send(embed=embedVar)
 
-    if message.content.startswith('r!randomquote'):
+    if message.content == ('r!quote'):
         quote = get_quote()
         await message.channel.send(quote)
 
-    if message.content.startswith('r!invite'):
+    if message.content == ('r!invite'):
         await message.channel.send(
             'https://discord.com/api/oauth2/authorize?client_id=809002048447184948&permissions=8&scope=bot'
         )
 
-    if message.content.startswith('r!website'):
+    if message.content == ('r!website'):
         await message.channel.send(
             'https://sites.google.com/view/registeeldiscordbot')
 
-    if message.content.startswith('r!yt'):
+    if message.content == ('r!yt'):
         await message.channel.send('Subscribe here! https://www.youtube.com/channel/UCxzkAP7o94jO-5U1DcESA8w')
 
-    if message.content.startswith('r!randomnumber'):
+    if message.content == ('r!randomnumber'):
       randomnumber = random.randint(0,1000)
       await message.channel.send(randomnumber)
       if randomnumber == 626:
         await message.channel.send('Your number is **626**. You win a cookie :)')
 
-    if message.content.startswith('r!sourcecode'):
+    if message.content == ('r!sourcecode'):
         await message.channel.send(
             'https://github.com/theultimatepokemontrainer/registeeldiscordbot'
         )
 
-    if message.content.startswith('r!coinflip'):
+    if message.content == ('r!coinflip'):
         embedVar = discord.Embed(title="Coin Flipped!", description=random, color=0xf1c40f,)
         await message.channel.send(embed=embedVar)
 
-    if message.content.startswith('r!vote'):
+    if message.content == ('r!vote'):
         await message.channel.send('You can vote every 12 hours! https://top.gg/bot/809002048447184948/vote')
 
-    if message.content.startswith('r!poll'):
+    if message.content == ('r!poll'):
         await message.add_reaction('👍')
         await message.add_reaction('👎')
         await message.channel.send('👍 for yes 👎 for no')
 
-    if message.content.startswith("r!say"):
+    if message.content == ("r!say"):
         await message.channel.send(message.content[5:].format(message))
 
-    if message.content.startswith('r!fact'):
+    if message.content == ('r!fact'):
         x = getFact()
         embed = discord.Embed(title='{}, here is your fact'.format(message.author.name), description=x, color=0x1abc9c)
         await message.channel.send(embed=embed)
+
+    if message.content == ("r!goto taj mahal"):
+        await message.channel.send('https://cdn.britannica.com/86/170586-050-AB7FEFAE/Taj-Mahal-Agra-India.jpg')
+
+    if message.content == ("r!goto colosseum"):
+        await message.channel.send('https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/250px-Colosseo_2020.jpg')
+
+    if message.content == ("r!goto chichen itza"):
+        await message.channel.send('https://cdn.hswstatic.com/gif/chichen-itza.jpg')
+
+    if message.content == ("r!goto machu picchu"):
+        await message.channel.send('https://i.guim.co.uk/img/media/b56952349419f749667d43f38b4d05e2980821a2/0_176_6016_3611/master/6016.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=d1d59c0c9b0e70e1da5989902cf5f01c')    
+
+    if message.content == ("r!goto Christ the Redeemer"):
+        await message.channel.send('http://4.bp.blogspot.com/-yA1inFreZAQ/Uei0hFMHFsI/AAAAAAAAHMY/rTic90zSDmk/s1600/BrasilCorcovadoStatue.jpg')
+
+    if message.content == ("r!goto petra"):
+        await message.channel.send('https://travelsquire.com/ts/wp-content/uploads/2019/10/Petra-Feature.jpg')
+
+    if message.content == ("r!goto great wall of china"):
+        await message.channel.send('https://cdn.getyourguide.com/img/location/5457947b8a235.jpeg/92.jpg')
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
